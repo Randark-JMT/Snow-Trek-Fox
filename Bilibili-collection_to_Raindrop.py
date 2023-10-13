67e90ee5f0622e4fb40ce03372e388efb15c9142
Randark_JMT <randark@foxmail.com>
2023-10-13 15:17:55 +0000

    init

---
+++ Bilibili-collection_to_Raindrop.py
@@ -0,0 +1,63 @@
+import json
+from raindropiopy import API, Collection, CollectionRef, Raindrop
+import requests
+
+
+Raindrop_api = API("892be2ec-cf85-4a2a-a1bd-51f9ba19140a")
+Bilibili_collection_url = "https://api.bilibili.com/x/v3/fav/resource/list"
+Bilibili_collection_del_url = "https://api.bilibili.com/x/v3/fav/resource/batch-del"
+with open("./Bilibili-cookie.txt", "r") as f:
+    Bilibili_cookie = f.read()
+
+
+for collection in Collection.get_collections(Raindrop_api):
+    if collection.title == "Bilibili 收藏":
+        collection_target = collection
+
+Bilibili_collection_id = "1594457495"
+
+while True:
+    response = requests.get(
+        Bilibili_collection_url,
+        params={
+            "media_id": Bilibili_collection_id,
+            "order": "mtime",
+            "ps": 20,
+            "keyword": "",
+            "platform": "web",
+            "order": "mtime",
+            "type": 0,
+            "tid": 0,
+            "pn": 0,
+        },
+        cookies={"SESSDATA": Bilibili_cookie},
+    )
+    for i in json.loads(response.text)["data"]["medias"]:
+        print(i["title"])
+        print("https://www.bilibili.com/video/{}".format(i["bvid"]))
+        print(i["cover"])
+        # print(i["intro"])
+        print("\n\n")
+
+        raindrop = Raindrop.create_link(
+            Raindrop_api,
+            collection=collection_target,
+            link="https://www.bilibili.com/video/{}".format(i["bvid"]),
+            title=i["title"],
+            excerpt=i["intro"],
+            cover=i["cover"],
+        )
+        print(f"Done, id={raindrop.id}")
+
+        data = {
+            "resources": "{}:{}".format(i["id"], i["type"]),
+            "media_id": Bilibili_collection_id,
+            "platform": "web",
+            "csrf": "bcec0a47fd3ee275a1078ba1c502a9b2",
+        }
+
+        response = requests.post(
+            Bilibili_collection_del_url, data=data, cookies={"SESSDATA": Bilibili_cookie}
+        )
+
+        print("del done" + response.text)
---
+++ README.md
@@ -0,0 +1,11 @@
+# raindrop_with_life
+
+一个私人向项目，目的是将互联网收藏夹/足迹进行提取，并将数据保存于 Raindrop 或导出为离线文件
+
+目前开发目标：
+
+1. Bilibili收藏夹 -> Raindrop
+
+## 参考
+
+[atsuoishimoto / python-raindropio](https://github.com/atsuoishimoto/python-raindropio#usage)
